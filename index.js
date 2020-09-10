594b8e5164e5bc25f6d28d68d59546d3d772a4fd
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-10 21:39:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function takeANumber(katzDeli, name ){
+  katzDeli.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0 ){
+    return("There is nobody waiting to be served!");
+  } else {
+    return ("Currently serving " + katzDeliLine.shift()+ ".");
+  }
+  
+  
+}
+
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return("The line is currently empty.");
+  }
+  
+}
