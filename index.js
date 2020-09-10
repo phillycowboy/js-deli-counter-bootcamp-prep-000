ecb0821142aca98265533a0752c50d2e100a47a6
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-10 21:57:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function takeANumber(katzDeli, name ){
   return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
 
+
+
 function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0 ){
     return("There is nobody waiting to be served!");
@@ -13,6 +15,8 @@ function nowServing(katzDeliLine){
   
   
 }
+
+
 
 function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
