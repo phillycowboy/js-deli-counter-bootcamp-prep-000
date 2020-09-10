fae7644de04345951f0eb9e129e6580afb50d714
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-10 22:21:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
     return("The line is currently empty.");
   }if (katzDeliLine.length>0){
-    var finalList=("The line is currently: ")
+    var finalList=("The line is currently:")
     for(let i = 0; i<katzDeliLine.length; i++){
-      finalList+=`${i+ 1}. ${katzDeliLine[i]}, `
+      finalList+=` ${i+ 1}. ${katzDeliLine[i]},`
     
       
     } 
